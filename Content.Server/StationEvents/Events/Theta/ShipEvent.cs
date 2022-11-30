61ce62e7889add23ec24cd1274f00e46a12413cb
KIBORG <bossmira4@gmail.com>
2022-11-30 23:40:48 +0300

    Ивент для спавна шип-ебента.
    Пиздец, savemap при сохранение у компоненты MetaData делает parent: 0, а savegrid делает parent: null. Как я понял, именно ЭТО изменение заставляет линтер НЕ спавнить в пушках патроны. Хз как я пофиксил, надеюсь линтер в CI не пошлёт.

---
+++ Content.Server/StationEvents/Events/Theta/ShipEvent.cs
@@ -0,0 +1,49 @@
+using Robust.Server.GameObjects;
+using Robust.Server.Maps;
+using Robust.Shared.Map;
+using Robust.Shared.Random;
+
+namespace Content.Server.StationEvents.Events.Theta;
+
+public sealed class ShipEvent : StationEventSystem
+{
+    [Dependency] private readonly MapLoaderSystem _map = default!;
+
+    public static List<string> ShipGrids = new()
+    {
+        "/Maps/Shuttles/ship_test_1.yml"
+    };
+
+    public override string Prototype => "ShipEvent";
+
+    public override void Started()
+    {
+        base.Started();
+
+        Vector2i? lastDir = null;
+        for (var i = 0; i < 2; i++)
+        {
+            if (TryFindRandomTile(out _, out _, out var targetGrid, out var targetTile))
+            {
+                var mapPos = (Vector2i) targetTile.ToMapPos(EntityManager);
+                if (lastDir is null)
+                {
+                    lastDir = RobustRandom.NextAngle().GetDir().ToIntVec();
+                }
+                else
+                {
+                    lastDir = lastDir.Value * -1; // invert vector
+                }
+                mapPos += lastDir.Value * RobustRandom.Next(500, 750);
+                var mapLoadOptions = new MapLoadOptions
+                {
+                    Rotation = RobustRandom.NextAngle(),
+                    Offset = mapPos,
+                    LoadMap = false,
+                };
+                _map.Load(Transform(targetGrid).MapID, RobustRandom.Pick(ShipGrids), mapLoadOptions);
+                Sawmill.Info($"Spawning the ship test shuttle at {targetTile}");
+            }
+        }
+    }
+}
