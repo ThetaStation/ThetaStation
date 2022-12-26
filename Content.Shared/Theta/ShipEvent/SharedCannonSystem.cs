b1b6bb99e31bb3d8d0ed468d20c29baa8a607367
KIBORG <bossmira4@gmail.com>
2022-12-26 17:41:52 +0300

    красоты

--- Content.Shared/Theta/ShipEvent/SharedCannonSystem.cs
+++ Content.Shared/Theta/ShipEvent/SharedCannonSystem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using Content.Shared.CombatMode;
 using Content.Shared.Physics;
+using Content.Shared.Weapons.Ranged.Components;
 using Content.Shared.Weapons.Ranged.Systems;
 using Robust.Shared.Map;
 using Robust.Shared.Physics;
@@ -24,21 +25,9 @@ public abstract class SharedCannonSystem : EntitySystem
     private void OnShootRequest(RequestCannonShootEvent ev, EntitySessionEventArgs args)
     {
         var gun = _gunSystem.GetGun(ev.Cannon);
-        if (gun == null || !_gunSystem.CanShoot(gun))
-            return;
-
-        var cannonTransform = Transform(ev.Cannon);
-        var dir = ev.Coordinates - cannonTransform.WorldPosition;
-        var ray = new CollisionRay(cannonTransform.WorldPosition, dir.Normalized, (int) (CollisionGroup.Impassable));
-
-        var averageShipLength = 25;
-
-        var rayCastResult = _physics.IntersectRay(cannonTransform.MapID, ray, averageShipLength).ToList();
-
-        foreach (var result in rayCastResult)
+        if (gun == null || !CanShoot(ev, gun))
         {
-           if (Transform(result.HitEntity).GridUid == cannonTransform.GridUid)
-               return;
+            return;
         }
 
         if (TryComp<SharedCombatModeComponent>(ev.Cannon, out var combatMode))
@@ -46,9 +35,31 @@ public abstract class SharedCannonSystem : EntitySystem
             combatMode.IsInCombatMode = true;
         }
 
-        var coords = EntityCoordinates.FromMap(ev.Cannon, new MapCoordinates(ev.Coordinates, cannonTransform.MapID));
+        var coords = EntityCoordinates.FromMap(ev.Cannon, new MapCoordinates(ev.Coordinates, Transform(ev.Cannon).MapID));
         _gunSystem.AttemptShoot(ev.Pilot, gun, coords);
 
+    }
+
+    public bool CanShoot(RequestCannonShootEvent args, GunComponent gun)
+    {
+        if (!_gunSystem.CanShoot(gun))
+            return false;
+
+        var cannonTransform = Transform(args.Cannon);
+        var dir = args.Coordinates - cannonTransform.WorldPosition;
+        var ray = new CollisionRay(cannonTransform.WorldPosition, dir.Normalized, (int) (CollisionGroup.Impassable));
+
+        const int averageShipLength = 25;
+
+        var rayCastResult = _physics.IntersectRay(cannonTransform.MapID, ray, averageShipLength).ToList();
+
+        foreach (var result in rayCastResult)
+        {
+            if (Transform(result.HitEntity).GridUid == cannonTransform.GridUid)
+                return false;
+        }
+
+        return true;
     }
 
     private void OnStopShootRequest(RequestStopCannonShootEvent ev)
