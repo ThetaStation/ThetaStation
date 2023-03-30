bc4c3f66a817408a328283718444978d4c9c4dbe
IlinMihail <57045293+IlinMihail@users.noreply.github.com>
2023-03-30 19:06:29 +0300

    fix suggestion

--- Content.Server/Shuttles/Systems/ShuttleSystem.Impact.cs
+++ Content.Server/Shuttles/Systems/ShuttleSystem.Impact.cs
@@ -61,8 +61,8 @@ public sealed partial class ShuttleSystem
         _audio.Play(_shuttleImpactSound, Filter.Pvs(coordinates, rangeMultiplier: 4f, entityMan: EntityManager), coordinates, true, audioParams);
         
         var kineticEnergy = ourBody.Mass * Math.Pow(jungleDiff, 2) / 2;
-		var mapCoords = coordinates.ToMap(EntityManager)
-		var intensity = (float)(kineticEnergy*IntensityMultiplier)
+		var mapCoords = coordinates.ToMap(EntityManager);
+		var intensity = (float)(kineticEnergy*IntensityMultiplier);
         _expSys.QueueExplosion(mapCoords, ExplosionSystem.DefaultExplosionPrototypeId, intensity , 5f, 50f);
     }
 }
