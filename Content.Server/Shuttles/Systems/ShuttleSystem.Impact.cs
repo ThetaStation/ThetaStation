7bd9a7c15e62792c03934e9ad6bf07418c784eee
IlinMihail <57045293+IlinMihail@users.noreply.github.com>
2023-03-30 18:51:00 +0300

    Update Content.Server/Shuttles/Systems/ShuttleSystem.Impact.cs
    Co-authored-by: KIBORG04 <bossmira4@gmail.com>

--- Content.Server/Shuttles/Systems/ShuttleSystem.Impact.cs
+++ Content.Server/Shuttles/Systems/ShuttleSystem.Impact.cs
@@ -61,6 +61,6 @@ public sealed partial class ShuttleSystem
         _audio.Play(_shuttleImpactSound, Filter.Pvs(coordinates, rangeMultiplier: 4f, entityMan: EntityManager), coordinates, true, audioParams);
         
         var kineticEnergy = ourBody.Mass * Math.Pow(jungleDiff, 2) / 2;
-        _expSys.QueueExplosion(coordinates.ToMap(EntityManager), "Default", (float)(kineticEnergy*IntensityMultiplier), 5f, 50f);
+        _expSys.QueueExplosion(coordinates.ToMap(EntityManager), ExplosionSystem.DefaultExplosionPrototypeId, (float)(kineticEnergy*IntensityMultiplier), 5f, 50f);
     }
 }
