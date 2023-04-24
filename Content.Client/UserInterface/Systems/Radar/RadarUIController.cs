06bbed0c924177ca90c9e09e3eace90afaa5156c
KIBORG04 <bossmira4@gmail.com>
2023-04-24 19:26:38 +0300

    Всё работает

---
+++ Content.Client/UserInterface/Systems/Radar/RadarUIController.cs
@@ -0,0 +1,29 @@
+﻿using Content.Client.UserInterface.Systems.Gameplay;
+using Content.Client.UserInterface.Systems.Radar.Widgets;
+using Robust.Client.UserInterface.Controllers;
+
+namespace Content.Client.UserInterface.Systems.Radar;
+
+public sealed class RadarUIController : UIController
+{
+    private RadarGui? RadarGui => UIManager.GetActiveUIWidgetOrNull<RadarGui>();
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        var gameplayStateLoad = UIManager.GetUIController<GameplayStateLoadController>();
+        gameplayStateLoad.OnScreenLoad += OnScreenLoad;
+        gameplayStateLoad.OnScreenUnload += OnScreenUnload;
+    }
+
+    private void OnScreenLoad()
+    {
+        RadarGui!.Visible = true;
+    }
+
+    private void OnScreenUnload()
+    {
+        RadarGui!.Visible = false;
+    }
+}
