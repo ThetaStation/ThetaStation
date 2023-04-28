59571a2870a32c21ad016cb6e321aaba3c24f019
KIBORG04 <bossmira4@gmail.com>
2023-04-28 17:16:54 +0300

    радар тольок для радарнуных энтити

--- Content.Client/UserInterface/Systems/Radar/RadarUIController.cs
+++ Content.Client/UserInterface/Systems/Radar/RadarUIController.cs
@@ -1,16 +1,24 @@
 ﻿using Content.Client.UserInterface.Systems.Gameplay;
 using Content.Client.UserInterface.Systems.Radar.Widgets;
+using Content.Shared.Theta.RadarHUD;
+using Robust.Client.GameObjects;
+using Robust.Client.Player;
+using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controllers;
 
 namespace Content.Client.UserInterface.Systems.Radar;
 
 public sealed class RadarUIController : UIController
 {
+    [Dependency] private readonly IPlayerManager _playerManager = default!;
+
     private RadarGui? RadarGui => UIManager.GetActiveUIWidgetOrNull<RadarGui>();
 
     public override void Initialize()
     {
-        base.Initialize();
+        SubscribeLocalEvent<RadarHudComponentAdded>(OnRadarHudAdded);
+        SubscribeLocalEvent<RadarHudComponentRemoved>(OnRadarHudRemoved);
+        SubscribeLocalEvent<PlayerAttachedEvent>(OnPlayerAttach);
 
         var gameplayStateLoad = UIManager.GetUIController<GameplayStateLoadController>();
         gameplayStateLoad.OnScreenLoad += OnScreenLoad;
@@ -19,11 +27,37 @@ public sealed class RadarUIController : UIController
 
     private void OnScreenLoad()
     {
-        RadarGui!.Visible = true;
+        UpdateGui();
     }
 
     private void OnScreenUnload()
     {
-        RadarGui!.Visible = false;
+        UpdateGui();
+    }
+
+    private void OnPlayerAttach(PlayerAttachedEvent ev)
+    {
+        UpdateGui();
+    }
+
+    private void UpdateGui()
+    {
+        if (RadarGui == null)
+            return;
+        RadarGui.Visible = EntityManager.HasComponent<RadarHUDComponent>(_playerManager.LocalPlayer?.ControlledEntity);
+    }
+
+    private void OnRadarHudAdded(ref RadarHudComponentAdded ev)
+    {
+        if (RadarGui == null)
+            return;
+        RadarGui.Visible = true;
+    }
+
+    private void OnRadarHudRemoved(ref RadarHudComponentRemoved ev)
+    {
+        if (RadarGui == null)
+            return;
+        RadarGui.Visible = false;
     }
 }
