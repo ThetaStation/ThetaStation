97a2c99d231081186d21c20ee9c5264cc8444b28
KIBORG04 <bossmira4@gmail.com>
2023-06-09 01:28:38 +0300

    mobs (не тестил)

---
+++ Content.Client/Theta/ModularRadar/Modules/RadarMobs.cs
@@ -0,0 +1,34 @@
+﻿using Content.Shared.Shuttles.BUIStates;
+using Robust.Client.Graphics;
+
+namespace Content.Client.Theta.ModularRadar.Modules;
+
+public sealed class RadarMobs : RadarModule
+{
+    private List<MobInterfaceState> _mobs = new();
+
+    public RadarMobs(ModularRadarControl parentRadar) : base(parentRadar)
+    {
+    }
+
+    public override void UpdateState(BoundUserInterfaceState state)
+    {
+        if (state is not RadarConsoleBoundInterfaceState radarState)
+            return;
+        _mobs = radarState.MobsAround;
+    }
+
+    public override void Draw(DrawingHandleScreen handle, Parameters parameters)
+    {
+        foreach (var state in _mobs)
+        {
+            var position = state.Coordinates.ToMapPos(EntManager);
+            var uiPosition = parameters.DrawMatrix.Transform(position);
+            var color = Color.Red;
+
+            uiPosition.Y = -uiPosition.Y;
+
+            handle.DrawCircle(ScalePosition(uiPosition), 3f, color);
+        }
+    }
+}
