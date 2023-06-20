c8b846d3a170bf4d147691c1664b3f8dbc25e920
IlinMihail <57045293+IlinMihail@users.noreply.github.com>
2023-06-20 13:17:39 +0200

    Более красивый оверлей при выходе за границы поля (#205)
    * 0
    
    * Delete border.png
    
    * Update Resources/Prototypes/Shaders/shaders.yml
    
    Co-authored-by: KIBORG04 <bossmira4@gmail.com>
    
    * rename
    
    * removed reference to border tex
    
    ---------
    
    Co-authored-by: KIBORG04 <bossmira4@gmail.com>

--- Content.Client/Theta/ShipEvent/Systems/BoundsOverlay.cs
+++ Content.Client/Theta/ShipEvent/Systems/BoundsOverlay.cs
@@ -1,9 +1,11 @@
 using Robust.Client.GameObjects;
 using Robust.Client.Graphics;
 using Robust.Client.Player;
+using Robust.Client.ResourceManagement;
 using Robust.Shared.Enums;
 using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
 
 namespace Content.Client.Theta.ShipEvent.Systems;
 
@@ -11,26 +13,36 @@ public sealed class BoundsOverlay : Overlay
 {
     [Dependency] private readonly IEntityManager _entMan = default!;
     [Dependency] private readonly IPrototypeManager _protMan = default!;
+    [Dependency] private readonly IResourceCache _resCache = default!;
     [Dependency] private readonly IPlayerManager _playerMan = default!;
     private readonly TransformSystem _formSys;
-    private ShaderInstance _monoColorShader;
     
+    private ShaderInstance _boundsShader;
+
     public override bool RequestScreenTexture => true;
     public override OverlaySpace Space => OverlaySpace.WorldSpace;
+
+    public DateTime LastTime;
+    public float SecondsOutsideBounds;
+    public const float FadeInTime = 1.5f;
+
     public Box2 Bounds;
     public MapId TargetMap;
+    public const float BorderWidth = 0.5f;
 
     public BoundsOverlay()
     {
         IoCManager.InjectDependencies(this);
         _formSys = _entMan.System<TransformSystem>();
-        _monoColorShader = _protMan.Index<ShaderPrototype>("MonoColor").InstanceUnique();
+        _boundsShader = _protMan.Index<ShaderPrototype>("BoundsOverlay").InstanceUnique();
     }
-    
+
     protected override void Draw(in OverlayDrawArgs args)
     {
         if (_entMan.TryGetComponent<TransformComponent>(_playerMan.LocalPlayer?.ControlledEntity, out var form))
         {
+            SecondsOutsideBounds += (float) (DateTime.Now - LastTime).TotalSeconds;
+
             if (form.MapID != TargetMap)
                 return;
 
@@ -38,16 +50,23 @@ public sealed class BoundsOverlay : Overlay
             {
                 if (ScreenTexture == null)
                     return;
-                
-                _monoColorShader.SetParameter("SCREEN_TEXTURE", ScreenTexture);
-                _monoColorShader.SetParameter("UCOLOR", new Vector3(255,0,0));
-                _monoColorShader.SetParameter("MIX", 0.1f);
-                args.WorldHandle.UseShader(_monoColorShader);
+
+                _boundsShader.SetParameter("SCREEN_TEXTURE", ScreenTexture);
+                _boundsShader.SetParameter("BRIGHTNESS", Math.Clamp(SecondsOutsideBounds / FadeInTime, 0, 1));
+
+                args.WorldHandle.UseShader(_boundsShader);
                 args.WorldHandle.DrawRect(args.WorldBounds, Color.White);
+                args.WorldHandle.UseShader(null);
+            }
+            else
+            {
+                SecondsOutsideBounds = 0;
             }
         }
         
-        args.WorldHandle.DrawRect(Bounds.Enlarged(0.5f), Color.Red, false);
+        args.WorldHandle.DrawRect(new Box2(Bounds.Left - BorderWidth, Bounds.Bottom - BorderWidth, Bounds.Right - BorderWidth, Bounds.Top - BorderWidth), Color.Red, false);
         args.WorldHandle.DrawRect(Bounds, Color.Red, false);
+
+        LastTime = DateTime.Now;
     }
 }
