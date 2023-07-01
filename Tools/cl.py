da185a08fd455f7e40d7109e8ea607bb54455544
IlinMihail <57045293+IlinMihail@users.noreply.github.com>
2023-07-01 16:12:00 +0200

    Генерация ченджлога (#211)
    * workflow, script, dependencies
    
    * Update cl.py
    
    * rename secret

---
+++ Tools/cl.py
@@ -0,0 +1,62 @@
+#changelog generator. triggered by action, when something is pushed into master, posting parsed PR's description to webhook.
+
+from os import environ
+from requests import post
+from github import Github
+from github.Commit import Commit
+from github.PullRequest import PullRequest
+
+try:
+    hookUrl = environ.get("HOOK_URL")
+except KeyError:
+    raise RuntimeError("Hook URL is missing (HOOK_URL). Add it to the step environment.")
+
+g = Github()
+r = g.get_repo("ThetaStation/ThetaStation")
+
+def getLatestCommit() -> Commit:
+    return r.get_branch("main").commit
+
+def getPullRequestByCommit(commit: Commit) -> PullRequest:
+    pulls = commit.get_pulls()
+    if pulls.totalCount == 0: return None
+    return pulls[0]
+
+def parsePullRequestDesc(desc: str) -> str:
+    tags = {("add","new"):":new:",
+            ("del","delete","remove"):":wastebasket:",
+            tuple(["fix"]):":wrench:",
+            ("mod","modify","tweak"):":scales:"}
+
+    result = ""
+    lines = desc.split("\n")
+
+    for i, line in enumerate(lines):
+        if line.startswith("🆑"):
+            result += line + "\n"
+            for ti, tagLine in enumerate(lines[i+1:]):
+                if len(tagLine) == 0 or tagLine.isspace(): continue
+
+                tagFound = False
+                for tag, prefix in tags.items():
+                    for variant in tag:
+                        if tagLine.startswith(variant+":"):
+                            result += prefix + " " + tagLine[len(variant)+1:] + "\n"
+                            tagFound = True
+                if not tagFound: raise RuntimeError(f"No tag found at line {i+ti+2}.")
+            break
+
+    return result
+
+latest = getLatestCommit()
+print(f"Latest commit: {latest.sha}")
+
+pr = getPullRequestByCommit(latest)
+if pr is None: raise RuntimeError("Latest commit does not belong to any pull request.")
+
+message = f"# {pr.title} (#{pr.number})\n{parsePullRequestDesc(pr.body)}"
+print(f"Changelog generated successfully.\n---\n{message}\n---")
+
+response = post(hookUrl, json={"content" : message})
+if response.ok: print("Successfully posted to hook.")
+else: print(f"Failed to post to hook, status code: {response.status_code}")
