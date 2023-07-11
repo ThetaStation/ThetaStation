80038bd53ae0678cdc1e5ef394582cf141212e98
IlinMihail <57045293+IlinMihail@users.noreply.github.com>
2023-07-11 22:12:31 +0300

    ну типо красивее

--- Content.Client/Theta/ModularRadar/Modules/ShipEvent/RadarLootboxTimer.cs
+++ Content.Client/Theta/ModularRadar/Modules/ShipEvent/RadarLootboxTimer.cs
@@ -10,29 +10,28 @@ namespace Content.Client.Theta.ModularRadar.Modules.ShipEvent;
 /// </summary>
 public sealed class RadarLootboxTimer : RadarModule
 {
-    private readonly IResourceCache resCache;
-    private readonly LootboxInfoSystem lootboxInfoSys;
-    private LootboxInfo? lootboxInfo;
+    [Dependency] private readonly IResourceCache _resCache = default!;
+    private readonly LootboxInfoSystem _lootboxInfoSys;
+    private LootboxInfo? _lootboxInfo;
     
-    private Font font;
-    private const int offsetX = 0;
-    private const int offsetY = -6;
+    private Font _font;
+    private const int OffsetX = 0;
+    private const int OffsetY = -6;
     
     private DateTime lastTime = DateTime.MinValue;
 
     public RadarLootboxTimer(ModularRadarControl parentRadar) : base(parentRadar)
     {
-        resCache = IoCManager.Resolve<IResourceCache>();
-        font = new VectorFont(resCache.GetResource<FontResource>("/Fonts/NotoSans/NotoSans-Regular.ttf"), 10);
+        _font = new VectorFont(_resCache.GetResource<FontResource>("/Fonts/NotoSans/NotoSans-Regular.ttf"), 10);
 
-        lootboxInfoSys = EntManager.System<LootboxInfoSystem>();
-        lootboxInfoSys.OnLootboxInfoReceived += UpdateLootboxInfo;
-        lootboxInfoSys.RequestLootboxInfo();
+        _lootboxInfoSys = EntManager.System<LootboxInfoSystem>();
+        _lootboxInfoSys.OnLootboxInfoReceived += UpdateLootboxInfo;
+        _lootboxInfoSys.RequestLootboxInfo();
     }
 
     public void UpdateLootboxInfo(LootboxInfo info)
     {
-        lootboxInfo = info;
+        _lootboxInfo = info;
     }
 
     public override void Draw(DrawingHandleScreen handle, Parameters parameters)
@@ -44,24 +43,24 @@ public sealed class RadarLootboxTimer : RadarModule
         float dt = (float)(DateTime.Now - lastTime).TotalSeconds;
         lastTime = DateTime.Now;
 
-        if (lootboxInfo == null)
+        if (_lootboxInfo == null)
             return;
         
-        for(int i = 0; i < lootboxInfo.Entities.Count; i++)
+        for(int i = 0; i < _lootboxInfo.Entities.Count; i++)
         {
-            Vector2 pos = lootboxInfo.Bounds[i].Center;
+            Vector2 pos = _lootboxInfo.Bounds[i].Center;
             
             pos = parameters.DrawMatrix.Transform(pos);
             pos.Y = -pos.Y;
             pos = ScalePosition(pos);
-            pos.X += offsetX;
-            pos.Y += offsetY;
+            pos.X += OffsetX;
+            pos.Y += OffsetY;
             
-            int minutes = (int)Math.Floor(lootboxInfo.Lifetime[i] / 60);
-            int seconds = (int)lootboxInfo.Lifetime[i] - minutes*60;
+            int minutes = (int)Math.Floor(_lootboxInfo.Lifetime[i] / 60);
+            int seconds = (int)_lootboxInfo.Lifetime[i] - minutes*60;
             
-            handle.DrawString(font, pos, $"{minutes:D2}:{seconds:D2}", Color.HotPink);
-            lootboxInfo.Lifetime[i] -= dt; //so we don't stop counting if we can't fetch new lootbox data rn
+            handle.DrawString(_font, pos, $"{minutes:D2}:{seconds:D2}", Color.HotPink);
+            _lootboxInfo.Lifetime[i] -= dt; //so we don't stop counting if we can't fetch new lootbox data rn
         }
     }
 }
