49e574d01928aeca10cc3844fef5cb3a7894ff43
KIBORG04 <bossmira4@gmail.com>
2023-08-07 20:03:14 +0300

    works

--- Content.Client/Theta/ShipEvent/Console/CircularShieldConsoleBUI.cs
+++ Content.Client/Theta/ShipEvent/Console/CircularShieldConsoleBUI.cs
@@ -3,14 +3,14 @@ using Content.Shared.Theta.ShipEvent.UI;
 using JetBrains.Annotations;
 using Robust.Client.GameObjects;
 
-namespace Content.Client.Theta.ShipEvent.UI;
+namespace Content.Client.Theta.ShipEvent.Console;
 
 
 [UsedImplicitly]
 public sealed class CircularShieldConsoleBoundUserInterface : BoundUserInterface
 {
     private CircularShieldConsoleWindow? _window;
-    
+
     public CircularShieldConsoleBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey) { }
 
     protected override void Open()
@@ -21,28 +21,24 @@ public sealed class CircularShieldConsoleBoundUserInterface : BoundUserInterface
         _window.OpenCentered();
         _window.OnClose += Close;
         _window.OnEnableButtonPressed += () => SendMessage(new CircularShieldToggleMessage());
-        _window.OnParametersChanged += () => UpdateShieldParameters();
-        
-        SendMessage(new CircularShieldConsoleInfoRequest());
+        _window.OnParametersChanged += UpdateShieldParameters;
     }
 
-    private void UpdateShieldParameters()
+    private void UpdateShieldParameters(int angle, int shieldWidth, int radius)
     {
-        if (_window == null)
-            return;
-
         SendMessage(new CircularShieldChangeParametersMessage(
-            Angle.FromDegrees(_window.Angle), 
-            Angle.FromDegrees(_window.Width),
-            _window.Radius));
+            Angle.FromDegrees(angle),
+            Angle.FromDegrees(shieldWidth),
+            radius));
     }
-    
+
     protected override void UpdateState(BoundUserInterfaceState state)
     {
-        if (!(state is CircularShieldConsoleWindowBoundsUserInterfaceState shieldSt))
+        if (state is not ShieldConsoleBoundsUserInterfaceState shieldSt)
             return;
-        
-        base.UpdateState(state);
+
+        _window?.SetMatrix(shieldSt.Coordinates, shieldSt.Angle);
+        _window?.SetOwner(Owner);
         _window?.UpdateState(shieldSt);
     }
 
