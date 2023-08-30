db4b0329e8d818fbfef35442e4fbcd92e9525265
IlinMihail <57045293+IlinMihail@users.noreply.github.com>
2023-08-30 23:24:02 +0300

    Update Content.Server/Theta/ShipEvent/Systems/ShipEventFactionSystem.cs
    Co-authored-by: KIBORG04 <bossmira4@gmail.com>

--- Content.Server/Theta/ShipEvent/Systems/ShipEventFactionSystem.cs
+++ Content.Server/Theta/ShipEvent/Systems/ShipEventFactionSystem.cs
@@ -163,7 +163,6 @@ public sealed partial class ShipEventFactionSystem : EntitySystem
         SubscribeAllEvent<LootboxInfoRequest>(OnLootboxInfoRequest);
 
         InitializeCaptainMenu();
-        
         SubscribeLocalEvent<RoundEndTextAppendEvent>(OnRoundEnd);
         SubscribeLocalEvent<RoundEndDiscordTextAppendEvent>(OnRoundEndDiscord);
         SubscribeLocalEvent<RoundRestartCleanupEvent>(OnRoundRestart);
