9022ca6f50ed790dc55462b4f958474fdbec4655
IlinMihail <57045293+IlinMihail@users.noreply.github.com>
2023-09-02 19:10:08 +0300

    Update StationEventSystem.cs

--- Content.Server/StationEvents/Events/StationEventSystem.cs
+++ Content.Server/StationEvents/Events/StationEventSystem.cs
@@ -140,7 +140,15 @@ public abstract class StationEventSystem<T> : GameRuleSystem<T> where T : Compon
 
         if (filter == null)
         {
-            stations.EnsureCapacity(Count<StationEventEligibleComponent>());
+            int stationCount = Count<StationEventEligibleComponent>();
+            
+            if (stationCount == 0)
+            {
+                station = null;
+                return false;
+            }
+
+            stations.EnsureCapacity(stationCount);
         }
 
         filter ??= _ => true;
