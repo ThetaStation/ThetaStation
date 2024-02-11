ef04255c989d8ddb407844ca4e845178bb0c6ced
IlinMihail <57045293+IlinMihail@users.noreply.github.com>
2024-02-11 15:13:08 +0300

    stealth cd from the start

--- Content.Server/Theta/ShipEvent/Systems/ShipEventFactionSystem.Stealth.cs
+++ Content.Server/Theta/ShipEvent/Systems/ShipEventFactionSystem.Stealth.cs
@@ -11,22 +11,40 @@ public partial class ShipEventFactionSystem
 {
     [Dependency] private ShuttleSystem _iffSys = default!;
 
-    private HashSet<EntityUid> OnCooldown = new ();
+    private HashSet<EntityUid> _onCooldown = new();
 
     private void InitializeStealth()
     {
+        SubscribeLocalEvent<ShipStealthComponent, ComponentInit>(OnStealthInit);
         SubscribeLocalEvent<ShuttleConsoleComponent, ShipEventToggleStealthMessage>(OnStealthActivated);
         SubscribeLocalEvent<ShuttleConsoleComponent, ShipEventRequestStealthStatusMessage>(OnStealthStatusRequest);
     }
 
-    public void OnStealthActivated(EntityUid uid, ShuttleConsoleComponent _, ShipEventToggleStealthMessage args)
+    private void OnStealthInit(EntityUid uid, ShipStealthComponent stealth, ComponentInit args)
     {
-        if (!TryComp<TransformComponent>(uid, out var xform) || xform.GridUid == null)
+        var xform = Transform(uid);
+        if (xform.GridUid == null)
             return;
 
         var shuttle = xform.GridUid.Value;
-        if (OnCooldown.Contains(shuttle)) return;
-        
+        _onCooldown.Add(shuttle);
+        Timer.Spawn((stealth.StealthDuration + stealth.StealthCooldown) * 1000, () =>
+        {
+            _onCooldown.Remove(shuttle);
+            RaiseNetworkEvent(new ShipEventStealthStatusMessage(true, EntityManager.GetNetEntity(uid)));
+        });
+    }
+
+    private void OnStealthActivated(EntityUid uid, ShuttleConsoleComponent _, ShipEventToggleStealthMessage args)
+    {
+        var xform = Transform(uid);
+        if (xform.GridUid == null)
+            return;
+
+        var shuttle = xform.GridUid.Value;
+        if (_onCooldown.Contains(shuttle))
+            return;
+
         if (!TryComp<ShipStealthComponent>(uid, out var stealth))
             return;
 
@@ -34,22 +52,22 @@ public partial class ShipEventFactionSystem
         _iffSys.AddIFFFlag(shuttle, IFFFlags.Hide);
         Timer.Spawn(stealth.StealthDuration * 1000, () => { _iffSys.RemoveIFFFlag(shuttle, IFFFlags.Hide); });
 
-        OnCooldown.Add(shuttle);
+        _onCooldown.Add(shuttle);
         Timer.Spawn((stealth.StealthDuration + stealth.StealthCooldown) * 1000, () =>
         {
-            OnCooldown.Remove(shuttle);
+            _onCooldown.Remove(shuttle);
             RaiseNetworkEvent(new ShipEventStealthStatusMessage(true, EntityManager.GetNetEntity(uid)), args.Session);
         });
     }
-    
+
     private void OnStealthStatusRequest(EntityUid uid, ShuttleConsoleComponent _, ShipEventRequestStealthStatusMessage args)
     {
         if (!TryComp<TransformComponent>(uid, out var xform) || xform.GridUid == null)
             return;
 
         var shuttle = xform.GridUid.Value;
-        RaiseNetworkEvent(new ShipEventStealthStatusMessage(!OnCooldown.Contains(shuttle), EntityManager.GetNetEntity(uid)), args.Session);
-        
+        RaiseNetworkEvent(new ShipEventStealthStatusMessage(!_onCooldown.Contains(shuttle), EntityManager.GetNetEntity(uid)), args.Session);
+
         //todo: no idea why this doesn't work
         /*if (_uiSys.TryGetUi(uid, args.UiKey, out var bui))
             _uiSys.SetUiState(bui, new ShipEventStealthStatusMessage(!OnCooldown.Contains(shuttle)), args.Session);*/
