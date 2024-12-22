9ae313e3673bf66423f30134014d13e5bad34e6c
miha <57045293+ilinm1@users.noreply.github.com>
2024-12-22 18:47:41 +0100

    Изменения в палитру (#467)
    * what a fool I have been
    
    * new palette

--- Content.Server/Theta/NiceColors/ColorPalette.cs
+++ Content.Server/Theta/NiceColors/ColorPalette.cs
@@ -21,4 +21,17 @@ public abstract class ColorPalette
             _currentColorPos = 0;
         return color;
     }
+
+    //for debugging, for example thru https://lawlesscreation.github.io/hex-color-visualiser/
+    public void ColorsToConsole()
+    {
+        string result = "";
+
+        foreach (Color color in Palette)
+        {
+            result += color.ToHexNoAlpha() + "\n";
+        }
+
+        Logger.Info(result);
+    }
 }
