e6b3e2595b44770ddca7b0b87e5cf52d4893fd59
IlinMihail <57045293+ilinm1@users.noreply.github.com>
2024-12-22 18:41:02 +0100

    new palette

--- Content.Server/Theta/NiceColors/ColorPalettes/ShipEventPalette.cs
+++ Content.Server/Theta/NiceColors/ColorPalettes/ShipEventPalette.cs
@@ -6,6 +6,10 @@ public sealed class ShipEventPalette : ColorPalette
 {
     [Dependency] private readonly IRobustRandom _random = default!;
 
+    private const double MinLuminosity = 120;
+    private const double MaxLuminosity = 180;
+    private const double MinAverageDelta = 10;
+
     private List<Color> _palette;
     public override List<Color> Palette => _palette;
 
@@ -23,9 +27,17 @@ public sealed class ShipEventPalette : ColorPalette
         {
             if (color.AByte != 0xFF)
                 continue;
-            const double lightLuminosity = 255f / 2f;
-            if (color.RByte * 0.2126 + color.GByte * 0.7152 + color.BByte * 0.0722 < lightLuminosity)
+
+            //filtering out greyish colors
+            double rgbAvg = (color.RByte + color.GByte + color.BByte) / 3;
+            if (Math.Abs(color.RByte - rgbAvg) < MinAverageDelta)
                 continue;
+
+            //https://stackoverflow.com/questions/26233781/detect-the-brightness-of-a-pixel-or-the-area-surrounding-it
+            double perceivedLuminosity = color.RByte * 0.2126 + color.GByte * 0.7152 + color.BByte * 0.0722;
+            if (perceivedLuminosity < MinLuminosity || perceivedLuminosity > MaxLuminosity)
+                continue;
+
             colors.Add(color);
         }
 
